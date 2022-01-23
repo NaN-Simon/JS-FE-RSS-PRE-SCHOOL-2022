17f7590b9d9122cc45b1474df8e2ddbd4618b4d7
NaN-Simon <goosaimon@gmail.com>
2022-01-23 18:51:11 +0300

    feat: add slowly hiding menu-burger

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,8 +1,17 @@
 const headerBurger = document.querySelector('.header-burger')
 const navList = document.querySelector('.nav-list')
+const navLink = document.querySelectorAll('.nav-link')
 const body = document.querySelector('body')
 headerBurger.addEventListener('click',function(){
   headerBurger.classList.toggle('active')
   navList.classList.toggle('active')
   body.classList.toggle('lock')
 })
+
+navLink.forEach((el) => el.addEventListener('click',closeMenu))
+
+function closeMenu(){
+  if(event.target.classList.contains('nav-link')){
+    navList.classList.remove('active')
+  }
+}
