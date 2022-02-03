2b1797b5b75d86d7bcc1d51611ead09d58156064
NaN-Simon <goosaimon@gmail.com>
2022-02-03 22:18:53 +0300

    feat: add slider-progress

--- js30#1.2-audio-player/index.js
+++ js30#1.2-audio-player/index.js
@@ -1,65 +1,78 @@
 function c1(c) {
   console.log(c);
 }
-const supPlayer = document.querySelector('.sub-player')
+const supPlayer = document.querySelector(".sub-player");
 const audio = document.querySelector(".audio");
 const playPauseBtn = document.querySelector(".play-pause");
 const PlayPauseImgSrc = document.querySelector(".play-pause-img__src");
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
-const songTitle = document.querySelector('.song-title')
-const coverPng = document.querySelector('.cover-png')
-const durationTime = document.querySelector('.duration-time')
-const currentTime = document.querySelector('.current-time')
-const progressBar = document.querySelector('.progress-bar')
+const songTitle = document.querySelector(".song-title");
+const coverPng = document.querySelector(".cover-png");
+const durationTime = document.querySelector(".duration-time");
+const currentTime = document.querySelector(".current-time");
+const progressBar = document.querySelector(".progress-bar");
+const progress = document.querySelector(".progress");
+const background = document.querySelector(".background");
 
-const songs = ['Beyonce - Don\'t Hurt Yourself','assets audio dontstartnow']
+
+const songs = ["Beyonce - Don't Hurt Yourself", "assets audio dontstartnow"];
 let songIndex = 0;
 
-/* playBtn.addEventListener("click", () => {
-  playPause();
-});  */
-function loadSong(song){
+function loadSong(song) {
   songTitle.innerHTML = song;
-  audio.src = `./assets/music/${song}.mp3`
-  coverPng.src = `./assets/images/cover${songIndex}.png`
+  audio.src = `./assets/music/${song}.mp3`;
+  coverPng.src = `./assets/images/cover${songIndex}.png`;
+  background.src = `./assets/images/cover${songIndex}.png`;
 }
-loadSong(songs[songIndex])
+loadSong(songs[songIndex]);
 
 //play
 function playSong() {
-  supPlayer.classList.add('.play')
+  supPlayer.classList.add(".play");
   audio.play();
-  PlayPauseImgSrc.src = './assets/icons/pause.png'
+  PlayPauseImgSrc.src = "./assets/icons/pause.png";
 }
 function pauseSong() {
-  supPlayer.classList.remove('.play')
+  supPlayer.classList.remove(".play");
   audio.pause();
-  PlayPauseImgSrc.src = './assets/icons/play.png'
+  PlayPauseImgSrc.src = "./assets/icons/play.png";
 }
-playPauseBtn.addEventListener('click',()=>{
-  const isPlay = supPlayer.classList.contains('.play')
-  if(isPlay){
-    pauseSong()
-  } else{
-    playSong()
+playPauseBtn.addEventListener("click", () => {
+  const isPlay = supPlayer.classList.contains(".play");
+  if (isPlay) {
+    pauseSong();
+  } else {
+    playSong();
   }
-})
+});
+
 //next
-c1(songs.length)
-c1(songIndex+1)
-c1(songs.length==songIndex+1)
-next.addEventListener('click',()=>{
-  if(songs.length!=songIndex+1){
-    songIndex += 1;
-    loadSong(songs[songIndex])
+next.addEventListener("click", () => {
+  if (songIndex + 1 == songs.length) {
+    songIndex = 0;
+  } else {
+    songIndex ++;
   }
-  
-})
+  loadSong(songs[songIndex]);
+  playSong();
+});
+
 //prev
-prev.addEventListener('click',()=>{
-  if(songIndex != 0){
-    songIndex -= 1;
-    loadSong(songs[songIndex])
+prev.addEventListener("click", () => {
+  if (songIndex == 0) {
+    songIndex = songs.length - 1;
+  } else {
+    songIndex --;
   }
-})
+  loadSong(songs[songIndex]);
+  playSong();
+});
+
+//progress bar
+function updateProgress(e){
+  const {duration,currentTime} = e.srcElement
+  const progressProcent = (currentTime/duration)*100
+  progress.style.width = `${progressProcent}%`
+}
+audio.addEventListener('timeupdate',updateProgress)
