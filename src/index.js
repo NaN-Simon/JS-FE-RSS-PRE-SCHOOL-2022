72b1677e84af0024c0be197a295f965bf40bb7a1
NaN-Simon <goosaimon@gmail.com>
2022-02-07 19:41:42 +0300

    feat: ready

--- src/index.js
+++ src/index.js
@@ -37,10 +37,94 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+/* function decode(expr) {
+    let result = ""
+    for (let i = 0;i<expr.length;i++){
+       for (let key in MORSE_TABLE) {
+        console.log()
+        if (expr[i] === MORSE_TABLE[key]){result += translate(key)}
+    }}
+    return result
+}
+function translate(key){
+    let result = ""
+    for (let letter of key){
+        if (letter == '.'){result += 10}
+        if (letter == '-'){result += 11}
+    }
+    return result.padStart(10,'0')
+} */
+//type2
 function decode(expr) {
-    // write your solution here
+    let result = ""
+    expr = Array.from(expr)
+    arr = toDivide(expr,10)
+    //con(arr)
+    arr.forEach(element => {
+        //con(element)
+        let arr2 = toDivide(element,2)
+        let arr3 = []
+        //con(arr2)
+        for (let duo of arr2){
+            //con(duo)
+            let solo = duo.join("")
+            if(solo != '00'){
+                if(solo == '**'){arr3.push(' ')}else{arr3.push(solo)}
+                
+            }
+        }
+        //con(arr3)
+        //con(translate(arr3))
+        result += convert(translate(arr3))
+        
+    });
+    return result
+}
+function toDivide(arrTen,s){
+    let arr = []
+    let size = s;
+    for (let i=0;i<Math.ceil(arrTen.length/size);i++){
+        arr[i]= arrTen.slice((i*size),(i*size)+size);
+    }
+    return arr
+}
+function translate(key){
+    let result = ""
+    key.forEach(el =>{
+        if (el == 10){result += '.'}
+        if (el == 11){result += '-'}    
+    })
+
+    return result
+}
+function convert(symb){
+    for (let key in MORSE_TABLE) {
+        if(symb == ""){return " "}
+        if(symb == key){
+            return MORSE_TABLE[key] }
+    }
+
 }
 
+
+
+
+
+
+
+
+
+
+
+/* const expr = '000000001100101010100000000010**********00111110110000101011000000101000111011100000111011**********00111010100000101110000011111100001011110000001110**********001010111000001111110011101011**********00101111110000101011000000111100101111100000101010**********0000111111001010101100000000100000101110**********000000001100101010100000000010**********0010111010000000101100111110100011101111**********000011101000001111110000111110'
+ */
+/* con(decode(expr))
+
+function con(c){
+    console.log(c)
+} */
 module.exports = {
     decode
 }
+
+
