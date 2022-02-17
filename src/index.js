c357d3a5896f68291bd51c2ecd01435f341945ad
NaN-Simon <goosaimon@gmail.com>
2022-02-17 18:27:05 +0300

    feat: ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
+const OPEN_BRACKETS = ["(", "{", "[", "1", "3", "5"];
+const BRACKETS_PAIR = {
+  ")": "(",
+  "}": "{",
+  "]": "[",
+  2: "1",
+  4: "3",
+  6: "5",
+};
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    let symb = str[i];
+    //console.log(symb)
+    if (
+      OPEN_BRACKETS.includes(symb) ||
+      ((symb === "|" || symb === "7" || symb === "8") && !stack.includes(symb))
+    ) {
+      stack.push(symb);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      const topEl = stack[stack.length - 1];
+      if (
+        BRACKETS_PAIR[symb] === topEl ||
+        symb === "|" ||
+        symb === "7" ||
+        symb === "8"
+      ) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
